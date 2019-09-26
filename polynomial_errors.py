d9a4df300c523733d9fe94d546fd4a9d2815ef58
Noel Evans <noelevans@gmail.com>
2019-09-26 10:21:48 +0100

    Looks better with linear smoothing with later figs

--- polynomial_errors.py
+++ polynomial_errors.py
@@ -4,12 +4,12 @@ import pandas as pd
 from scipy import interpolate, poly1d
 import matplotlib.pyplot as plt
 import warnings
- 
- 
+
+
 def rmse(targets, predictions):
     return np.sqrt(((targets - predictions) ** 2).mean())
-   
- 
+
+
 def model(xs, ys, n=3):
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', np.RankWarning)
@@ -26,11 +26,11 @@ def main():
     train_ys = train['mass change']
     test_xs  = test['year']
     test_ys  = test['mass change']
- 
+
     diffs = [rmse(train_ys, model(train_xs, train_ys, n))
              for n in range(1, 41)]
-    plt.plot(range(1, 41), diffs, '.')
-   
+    plt.plot(range(1, 41), diffs)
+
     plt.title('Error by polynomial degrees')
     plt.xlabel('Degrees')
     plt.ylabel('Error')
@@ -38,8 +38,8 @@ def main():
     plt.gca().set_xlim(0)
     plt.savefig('polynomial_errors.png')
     plt.show()
- 
- 
+
+
 if __name__ == '__main__':
     main()
 
